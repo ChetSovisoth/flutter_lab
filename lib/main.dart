aa32408b13d7aace6c867f287e1d82f34a036421
ChetSovisoth <chet.sovisoth@gmail.com>
2024-12-19 08:06:51 +0000

    remove product page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:lab/login_view.dart';
-import 'package:lab/product/product_view.dart';
+import 'package:lab/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: ProductView()
+      home: LoginView()
     );
   }
 }
